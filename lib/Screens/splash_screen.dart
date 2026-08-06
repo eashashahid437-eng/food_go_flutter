002c0a8734f80f51b8df7f54118202fcf3d3b490
mirzazainabjamil <mirzazainabjamil@gmail.com>
2026-08-06 13:24:55 +0500

    added structure

---
+++ lib/Screens/splash_screen.dart
@@ -0,0 +1,91 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'home_screen.dart';
+
+const String splashImage = 'assets/images/splash_image.png';
+const String burger1 = 'assets/images/burger1.png';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const HomeScreen(),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xffff8995),
+              Color(0xffff172d),
+            ],
+          ),
+        ),
+        child: Stack(
+          children: [
+
+            // Foodgo Logo
+            const Center(
+              child: Text(
+                "Foodgo",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+            ),
+
+            // Bottom burger
+            Positioned(
+              bottom: -10,
+              left: -20,
+              child: Image.asset(
+                splashImage,
+                width: 230,
+                height: 180,
+                fit: BoxFit.contain,
+              ),
+            ),
+
+            // Bottom right burger
+            Positioned(
+              bottom: -5,
+              right: -25,
+              child: Image.asset(
+                burger1,
+                width: 210,
+                height: 170,
+                fit: BoxFit.contain,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+}
